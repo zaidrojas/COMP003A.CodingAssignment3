7e0972a8049d8b86e2e0b097ffaad3532867caea
zaidrojas <ZaidNemesis83@outlook.com>
2025-01-29 10:30:40 -0800

    Add project files.

---
+++ COMP003A.CodingAssignment3/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+* [Zaid Rojas]
+* COMP-003A
+* Faculty: Jonnathan Cruz
+* Purpose: Budget management application demonstrating control flow
+*/
+
+namespace COMP003A.CodingAssignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialized Variables
+            int option;
+            double income;
+            //
+
+            Console.WriteLine("Welcome to the Budget Management Tool!");
+            Console.Write("\nEnter your monthly income: ");
+            income = double.Parse(Console.ReadLine());
+        }
+    }
+}
